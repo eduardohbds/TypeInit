2830f66875b570bfe3de9e155a5129070d4c5e76
Eduardo <eduardo.hbds@gmail.com>
2023-05-23 11:01:32 -0300

    chore: add npm,eslint

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let hello;
+hello = "Typescript is successfully installed" +
+    " onto this system. -GeeksforGeeks";
+console.log(hello);
---
+++ main.ts
@@ -0,0 +1,6 @@
+let hello:string;
+
+hello = "Typescript is successfully installed"+
+  " onto this system. -GeeksforGeeks";
+    
+console.log(hello);
