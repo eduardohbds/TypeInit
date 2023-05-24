503827698ebe8a499ff7837052282b11aedd9b4f
Eduardo Silva <eduardo.hbds@gmail.com>
2023-05-24 16:11:10 -0300

    feat: ensure SignUpController return an error if is missing passwordConfirmation

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -3,12 +3,11 @@ import { MissingParamError } from "../errors/missing-error-params"
 import { badrequest } from "../helpers/http-helper"
 export class SignUpController{
   handle(httpRequest: HttpRequest):HttpResponse{
-    const requiredField=['name','email','password']
+    const requiredField = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredField) { 
       if (!httpRequest.body[field]) { 
         return badrequest(new MissingParamError(field))
       }
     }
-
   }
 }
